d9193789d4b6c57c23becbc7b633c2f035132b3b
zhuoqianpeng <110985292+zhuoqianpeng@users.noreply.github.com>
2022-09-30 22:00:27 +0800

    MakeCode 项目的初始文件

---
+++ images.g.ts
@@ -0,0 +1,174 @@
+// 自动生成的代码。请勿编辑。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image4":
+            case "myImage2":return img`
+. . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 
+. f . . . . . . . . . . . . 
+. . f . . . . . . . f f f . 
+. . f f . . . . . . f 5 f f 
+. . . f . . . . . . f f f f 
+. . . f f f f f f f f . . . 
+. . . f f f f f f f f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 
+. . . . . . . . . . . . f . 
+. f f f . . . . . . . f . . 
+f f 5 f . . . . . . f f . . 
+f f f f . . . . . . f . . . 
+. . . f f f f f f f f . . . 
+. . . f f f f f f f f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+`;
+            case "image5":
+            case "myImage3":return img`
+. . . . . . . . . . . . . . . . 
+. f . . . . . . . . . . . . . . 
+. f . . . . . . . . . . . . . . 
+. f . . . . . . . . . . . . . . 
+. . f . . . . . . . . . . . . . 
+. . . f . . . . . . . . . . . . 
+. . . f f f . . . . . . . . . . 
+. . . f f f f . . . . . . . . . 
+. . . . f f f f f . . . . . . . 
+. . . . f . f f f f . . . . . . 
+. . . . f . . . f f f f . . . . 
+. . . . f . . . . f f f f f f . 
+. . . . f . . . . . f . f 5 f . 
+. . . . f . . . . . f . f f f . 
+. . . . . . . . . . f . f f . . 
+. . . . . . . . . . f . . . . . 
+`;
+            case "image6":
+            case "myImage4":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . f f f . 
+. . . . . . . . . . . . f 5 f f 
+. . . . . . . . . . . . f f f f 
+. . . . . . . . . . . f f . . . 
+. . . . . . . . . . f f f . . . 
+. . . . . . . . . f f f . . . . 
+. . . . . . . f f f f f f f f f 
+. . . . . . f f f f f f f . . . 
+. . . . . f f f . . . . . f f f 
+. . . . f f f f . . . . . . . . 
+. . . f f f f . f . . . . . . . 
+. . . . . . f . . f . . . . . . 
+. . . . . . f . . . f . . . . . 
+. . . . . . f . . . . . . . . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . . . f f . . . . . . . 
+. . . . f f f . . . . . . . 
+. . . . f 5 f . . . . . . . 
+. . . . f f f f f f f f f f 
+. . . . . . . f f . . . . . 
+. . . . . . . f f f f f f f 
+. . . . . . . f f . . . . . 
+. . . . . . . f f . . . . . 
+. . . . . . . f f f f f f f 
+. . . . . . . f f . . . . . 
+f f . . . f f f f f f f f f 
+. f . . f f . . . . . . . . 
+. . f f . . . . . . . . . . 
+. . . . . . . . . . . . . . 
+`;
+            case "image7":
+            case "myImage5":return img`
+. . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 
+. f . . . . . . . . f . . . 
+. . f . . . . . . . f f f . 
+. . f f . . . . . . f 5 f f 
+. . . f . . . . . . f f f f 
+. . . f f f f f f f f . . . 
+. . . f f f f f f f f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+. . . f . f . . f . f . . . 
+`;
+            case "image1":
+            case "myImage":return img`
+e e e . . . . e e e . . . . 
+c d d c . . c d d c . . . . 
+c b d d f f d d b c . . . . 
+c 3 b d d b d b 3 c . . . . 
+f b 3 d d d d 3 b f . . . . 
+e d d d d d d d d e . . . . 
+e d f d d d d f d e . b f b 
+f d d f d d f d d f . f d f 
+f b d d b b d d 2 f . f d f 
+. f 2 2 2 2 2 2 b b f f d f 
+. f b d d d d d d b b d b f 
+. f d d d d d b d d f f f . 
+. f d f f f d f f d f . . . 
+. f f . . f f . . f f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . e e e . . . . e e e 
+. . . . c d d c . . c d d c 
+. . . . c b d d f f d d b c 
+. . . . c 3 b d b d d b 3 c 
+. . . . f b 3 d d d d 3 b f 
+. . . . e d d d d d d d d e 
+b f b . e d f d d d d f d e 
+f d f . f d d f d d f d d f 
+f d f . f 2 d d b b d d b f 
+f d f f b b 2 2 2 2 2 2 f . 
+f b d b b d d d d d d b f . 
+. f f f d d b d d d d d f . 
+. . . f d f f d f f f d f . 
+. . . f d . . d f . . d f . 
+. . . f f . . f f . . f f . 
+`, img`
+. . . . e e e . . . . e e e 
+. . . . c d d c . . c d d c 
+. . . . c b d d f f d d b c 
+. . . . c 3 b d b d d b 3 c 
+. . . . f b 3 d d d d 3 b f 
+. . . . e d d d d d d d d e 
+b f b . e d f d d d d f d e 
+f d f . f d d f d d f d d f 
+f d f . f 2 d d b b d d b f 
+f d f f b b 2 2 2 2 2 2 f . 
+f b d b b d d d d d d b f . 
+. f f f d d b d d d d d f . 
+. . . f d f f d f f f d f . 
+. . . f f . . f f . . f f . 
+. . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
